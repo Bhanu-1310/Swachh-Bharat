e5cf61d6cc5088ca70c3c02e9e6892e8591c0a65
Bhanu-1310 <bhanuarora2506@gmail.com>
2021-05-01 13:01:51 +0530

    initial commit

---
+++ registercomp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def regicomplaint(request):
+    return render(request, 'complaint.html')
